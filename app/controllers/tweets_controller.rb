484791684e9873019ca5cfbe97a798bc0de22a52
syuji_yamamoto <51443836+syuji-yamamoto@users.noreply.github.com>
2020-07-04 01:09:59 +0900

    first commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,57 @@
+class TweetsController < ApplicationController
+  before_action :set_tweet1, only: [:show, :edit] 
+  before_action :set_tweet2, only: [:update, :destroy] 
+  before_action :move_to_index, except: [:index, :show, :search]
+
+	def index
+    @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page])
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    binding.pry
+    Tweet.create(tweet_params)
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @tweet.comments.includes(:user)
+  end
+
+  def edit
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+
+  def search
+    @tweets = Tweet.search(params[:keyword])
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:image, :text).merge(user_id: current_user.id)
+  end
+
+  def set_tweet1
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def set_tweet2
+    tweet = Tweet.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root "tweets#index"
+  resources :tweets do
+    collection do
+      get 'search'
+    end
+    resources :comments, only: :create
+  end
+  resources :users, only: :show
+end
