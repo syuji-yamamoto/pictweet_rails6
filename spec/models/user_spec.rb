9d3257b134d3f9ec3598f77dcea42705268dffc4
syuji_yamamoto <51443836+syuji-yamamoto@users.noreply.github.com>
2021-05-10 21:16:24 +0900

    リファクタリング

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,14 +32,32 @@ describe User do
         expect(@user.errors.full_messages).to include("Nickname is too long (maximum is 6 characters)")
       end
       it "emailが空では登録できない" do
+        @user.email = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
       end
       it "重複したemailが存在する場合登録できない" do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
       it "passwordが空では登録できない" do
+        @user.password = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
       end
       it "passwordが5文字以下であれば登録できない" do
+        @user.password = "aaaaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password", "Password is too short (minimum is 6 characters)")
       end
       it "passwordが存在してもpassword_confirmationが空では登録できない" do
+        @user.password = "111aaa"
+        @user.password_confirmation = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
     end
   end
